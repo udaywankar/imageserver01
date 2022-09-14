dc93e61366d0f687f833ff6521f0e6fb946e0d0b
uday wankar <udaywankar@gmail.com>
2022-09-14 12:17:00 +0530

    adding images

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require('express');
 const app = express();
 const multer = require("multer");
 const path = require("path");
-
+// var cors = require("cors");
+// app.use(cors());
 // storage engine 
-
+var qwer;
+global.myNumber;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+       res.sendStatus(200);
+     }
+     else {
+       next();
+     }});
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        console.log("11111111111111111111");
+        console.log(req.body);
+        console.log(req.body.prodid);
+        console.log(global.myNumber);
+        const { prodid } = req.body
+        console.log(prodid);
+        let prodId=12;
+        let imageNo=1;
+        //return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        return cb(null, `image_${prodId}_${imageNo}${path.extname(file.originalname)}`)
     }
 })
 
@@ -18,12 +39,17 @@ const upload = multer({
         fileSize: 10000000
     }
 })
-app.use('/profile', express.static('upload/images'));
-app.post("/upload", upload.single('profile'), (req, res) => {
+
+app.use('/image', express.static('upload/images'));
+app.post("/upload",upload.single('profile'),  (req, res) => {
+    console.log("0000000000000000000000");
+    
+    //console.log(req);
+    
 
     res.json({
         success: 1,
-        profile_url: `http://localhost:4000/profile/${req.file.filename}`
+        profile_url: `http://localhost:4000/image/${req.file.filename}`
     })
 })
 
@@ -35,7 +61,8 @@ function errHandler(err, req, res, next) {
         })
     }
 }
+
 app.use(errHandler);
 app.listen(4000, () => {
-    console.log("server up and running");
+    console.log("server up and running on port 4000");
 })
